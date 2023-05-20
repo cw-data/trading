d0fad4274370332b644d47be355d0877361ab040
cw-data <charles.a.wainright@gmail.com>
2023-05-20 11:01:19 -0400

    initializing project structure

---
+++ config_template.py
@@ -0,0 +1,11 @@
+# The format of the project's real config.py
+# config.py should remain private because it real Alpaca API keys
+
+ALPACA_KEY_ID = 'your_key'
+ALPACA_SECRET_KEY = 'your_secret'
+ALPACA_BASE_URL = 'https://paper-api.alpaca.markets'
+
+HEADERS = {
+    'APCA-API-KEY-ID': ALPACA_KEY_ID,
+    'APCA-API-SECRET-KEY': ALPACA_SECRET_KEY
+}
