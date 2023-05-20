7d442459fd0ca9726686698ba1553cbcee841692
cw-data <charles.a.wainright@gmail.com>
2023-05-20 18:11:21 -0400

    prototyping get and post methods

--- website/pages.py
+++ website/pages.py
@@ -1,7 +1,16 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 pages = Blueprint('pages', __name__)
 
-@ pages.route('/')
+@ pages.route('/', methods=['GET', 'POST'])
 def index():
+
+    if request.method == 'POST':
+        ticker = request.form.get('ticker')
+        qty = request.form.get('qty')
+        order_type = request.form.get('order_type')
+        print(ticker)
+        print(qty)
+        print(order_type)
+
     return render_template('index.html')
