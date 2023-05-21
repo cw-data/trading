862037a38451e735d16a2b765829cc1723d3268f
cw-data <charles.a.wainright@gmail.com>
2023-05-21 13:35:46 -0400

    prototyped candlestick chart with SMA

--- website/pages.py
+++ website/pages.py
@@ -1,8 +1,17 @@
 from flask import Blueprint, render_template, request
 from alpaca import create_market_order, MarketOrder
+import talib as ta
+from alpaca_trade_api import REST, TimeFrame
+import config
+import plotly.graph_objects as go
+import plotly.express as px
+import plotly.utils as pu
+import json
+import pandas as pd
 
 
 pages = Blueprint('pages', __name__)
+rest_client = REST(config.ALPACA_KEY_ID, config.ALPACA_SECRET_KEY)
 
 @ pages.route('/', methods=['GET', 'POST'])
 def index():
@@ -23,11 +32,33 @@ def index():
             order = MarketOrder(ticker, qty, order_side, order_type)
             print(order.data)
             order.place_order()
-            # create_market_order(ticker, qty, order_side, order_type)
             return render_template('index.html')
         elif order_type == 'limit':
             return render_template('index.html')
         else:
             print("error: didn't match an order_type")
 
-    return render_template('index.html')
+    rest_client = REST(config.ALPACA_KEY_ID, config.ALPACA_SECRET_KEY)
+    bars = rest_client.get_bars("SPY", TimeFrame.Day, "2021-06-01", "2021-10-01").df
+    bars['30_Day_SMA'] = ta.SMA(bars['close'], timeperiod=30)
+    candlestick_fig = go.Figure(data=[go.Candlestick(x=bars.index,
+                    open=bars['open'],
+                    high=bars['high'],
+                    low=bars['low'],
+                    close=bars['close'])])
+    sma_fig = px.line(x=bars.index, y=bars['30_Day_SMA'])
+    fig = go.Figure(data=candlestick_fig.data + sma_fig.data)
+    graphJSON = json.dumps(fig, cls=pu.PlotlyJSONEncoder)
+    # fig.show()
+    # df = pd.DataFrame(
+    #    {
+    #         'Fruit': ['Apples', 'Oranges', 'Bananas', 'Apples', 'Oranges', 'Bananas'],
+    #         'Amount': [4, 1, 2, 2, 4, 5],
+    #         'City': ['SF', 'SF', 'SF', 'Montreal', 'Montreal', 'Montreal']
+    #     }
+    # )
+    # fig = px.bar(df, x='Fruit', y='Amount', color='City', barmode='group')
+    # graphJSON = json.dumps(fig, cls=pu.PlotlyJSONEncoder)
+#    return render_template('notdash.html', graphJSON=graphJSON)
+
+    return render_template('index.html', graphJSON=graphJSON)
