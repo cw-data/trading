36ae554ba8ef1f2323ba38e93d31b5766c847d53
cw-data <charles.a.wainright@gmail.com>
2023-05-21 13:00:04 -0400

    prototype candlestick chart with 30-day SMA

---
+++ tests/sma_30day.py
@@ -0,0 +1,21 @@
+# https://alpaca.markets/learn/technical-analysis-with-ta-lib-and-market-data-api/
+# import market data via `alpaca_trade_api` and plot candlestick chart with 30-day simple moving average
+import talib as ta
+from alpaca_trade_api import REST, TimeFrame
+import config
+import plotly.graph_objects as go
+import plotly.express as px
+
+rest_client = REST(config.ALPACA_KEY_ID, config.ALPACA_SECRET_KEY)
+bars = rest_client.get_bars("SPY", TimeFrame.Day, "2021-06-01", "2021-10-01").df
+
+bars
+bars['30_Day_SMA'] = ta.SMA(bars['close'], timeperiod=30)
+candlestick_fig = go.Figure(data=[go.Candlestick(x=bars.index,
+                open=bars['open'],
+                high=bars['high'],
+                low=bars['low'],
+                close=bars['close'])])
+sma_fig = px.line(x=bars.index, y=bars['30_Day_SMA'])
+fig = go.Figure(data=candlestick_fig.data + sma_fig.data)
+fig.show()
