25684635fcdbc576706f21173a1a73b2f1480bab
cw-data <charles.a.wainright@gmail.com>
2023-05-22 19:13:56 -0400

    prototyping table addition to interface

--- alpaca.py
+++ alpaca.py
@@ -1,8 +1,20 @@
 import config
 import requests
 import json
+import talib as ta
+from alpaca_trade_api import REST, TimeFrame
+import config
+import plotly.graph_objects as go
+import plotly.express as px
+import plotly.utils as pu
+import json
+import pandas as pd
 
 ORDERS_URL = config.ALPACA_BASE_URL + '/v2/orders'
+rest_client = REST(config.ALPACA_KEY_ID, config.ALPACA_SECRET_KEY)
+bars = rest_client.get_bars("SPY", TimeFrame.Day, "2021-06-01", "2021-10-01").df
+bars['simple_ma'] = ta.SMA(bars['close'], timeperiod=30)
+bars['upper_band'], bars['middle_band'], bars['lower_band'] = ta.BBANDS(bars['close'], timeperiod=30)
 
 def create_market_order(ticker, qty, side, type):
     data = {
@@ -42,3 +54,38 @@ class MarketOrder(Order):
             'type': self.type,
             'time_in_force': 'gtc'
         }
+
+def MainFig() -> str:
+    """
+    """
+    # rest_client = REST(config.ALPACA_KEY_ID, config.ALPACA_SECRET_KEY)
+    # bars = rest_client.get_bars("SPY", TimeFrame.Day, "2021-06-01", "2021-10-01").df
+    # bars['30_Day_SMA'] = ta.SMA(bars['close'], timeperiod=30)
+    # bars['upper_band'], bars['middle_band'], bars['lower_band'] = ta.BBANDS(bars['close'], timeperiod=30)
+    candlestick_fig = go.Figure(data=[go.Candlestick(x=bars.index,
+                    open=bars['open'],
+                    high=bars['high'],
+                    low=bars['low'],
+                    close=bars['close'])])
+    sma_fig = px.line(x=bars.index, y=bars['simple_ma'])
+    upper_line_fig = px.line(x=bars.index, y=bars['upper_band'])
+    lower_line_fig = px.line(x=bars.index, y=bars['lower_band'])
+    fig = go.Figure(data=candlestick_fig.data + sma_fig.data + upper_line_fig.data + lower_line_fig.data)
+    fig.update_layout(xaxis_rangeslider_visible=False, template='plotly_dark', showlegend=False)
+    graphJSON = json.dumps(fig, cls=pu.PlotlyJSONEncoder)
+    return graphJSON
+
+def pos_table() -> str:
+    # https://plotly.com/python/table/
+    # df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/2014_usa_states.csv').head()
+    df = bars
+    fig = go.Figure(data=[go.Table(
+        header=dict(values=list(df.columns),
+                    fill_color='paleturquoise',
+                    align='left'),
+        cells=dict(values=[df.open, df.high, df.low, df.close, df.volume, df.trade_count, df.vwap, df.simple_ma, df.upper_band, df.middle_band, df.lower_band],
+                fill_color='lavender',
+                align='left'))
+    ])
+    graphJSON = json.dumps(fig, cls=pu.PlotlyJSONEncoder)
+    return graphJSON
--- website/pages.py
+++ website/pages.py
@@ -1,35 +1,14 @@
 from flask import Blueprint, render_template, request
-from alpaca import create_market_order, MarketOrder
-import talib as ta
-from alpaca_trade_api import REST, TimeFrame
+from alpaca import MarketOrder, MainFig, pos_table
 import config
-import plotly.graph_objects as go
-import plotly.express as px
-import plotly.utils as pu
-import json
-import pandas as pd
 
 pages = Blueprint('pages', __name__)
-rest_client = REST(config.ALPACA_KEY_ID, config.ALPACA_SECRET_KEY)
 
 @ pages.route('/', methods=['GET', 'POST'])
 def index():
 
-    rest_client = REST(config.ALPACA_KEY_ID, config.ALPACA_SECRET_KEY)
-    bars = rest_client.get_bars("SPY", TimeFrame.Day, "2021-06-01", "2021-10-01").df
-    bars['30_Day_SMA'] = ta.SMA(bars['close'], timeperiod=30)
-    bars['upper_band'], bars['middle_band'], bars['lower_band'] = ta.BBANDS(bars['close'], timeperiod =30)
-    candlestick_fig = go.Figure(data=[go.Candlestick(x=bars.index,
-                    open=bars['open'],
-                    high=bars['high'],
-                    low=bars['low'],
-                    close=bars['close'])])
-    sma_fig = px.line(x=bars.index, y=bars['30_Day_SMA'])
-    upper_line_fig = px.line(x=bars.index, y=bars['upper_band'])
-    lower_line_fig = px.line(x=bars.index, y=bars['lower_band'])
-    fig = go.Figure(data=candlestick_fig.data + sma_fig.data + upper_line_fig.data + lower_line_fig.data)
-    fig.update_layout(xaxis_rangeslider_visible=False, template='plotly_dark', showlegend=False)
-    graphJSON = json.dumps(fig, cls=pu.PlotlyJSONEncoder)
+    main_fig = MainFig()
+    main_table = pos_table()
 
     if request.method == 'POST':
         ticker = request.form.get('ticker')
@@ -41,10 +20,10 @@ def index():
             order = MarketOrder(ticker, qty, order_side, order_type)
             print(order.data)
             order.place_order()
-            return render_template('index.html', graphJSON=graphJSON)
+            return render_template('index.html', graphJSON=main_fig, main_table=main_table)
         elif order_type == 'limit':
-            return render_template('index.html', graphJSON=graphJSON)
+            return render_template('index.html', graphJSON=main_fig, main_table=main_table)
         else:
             print("error: didn't match an order_type")
 
-    return render_template('index.html', graphJSON=graphJSON)
+    return render_template('index.html', graphJSON=main_fig, main_table=main_table)
