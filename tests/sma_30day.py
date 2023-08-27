87a4df3276ca0c56b2956728e42e484bc1796804
cw-data <charles.a.wainright@gmail.com>
2023-08-27 14:42:02 -0400

    added bollinger band position finding function

--- tests/sma_30day.py
+++ tests/sma_30day.py
@@ -6,6 +6,8 @@ import config
 import plotly.graph_objects as go
 import plotly.express as px
 import pandas as pd
+import time
+import numpy as np
 
 rest_client = REST(config.ALPACA_KEY_ID, config.ALPACA_SECRET_KEY)
 bars = rest_client.get_bars("SPY", TimeFrame.Day, "2021-06-01", "2021-10-01").df
@@ -27,56 +29,32 @@ fig = go.Figure(data=candlestick_fig.data + sma_fig.data + upper_line_fig.data +
 fig.show()
 
 # Identify positions
-positions = pd.DataFrame
-# find points where the close was above the upper band
-closes = {
-    'pos_type': [],
-    'timestamp_in': [],
-    'close': [],
-    'upper': []
-}
-for i in range(len(bars['open'])):
-    if bars['close'][i] > bars['upper_band'][i]:
-        myclose = bars['close'][i]
-        myband = bars['upper_band'][i]
-        mytimestamp = bars['timestamp_in'][i]
-        # print('got here')
-        closes['pos_type'].append('sell')
-        closes['timestamp'].append(mytimestamp)
-        closes['close'].append(myclose)
-        closes['upper'].append(myband)
-        
 
+def find_bband_positions(df:pd.DataFrame) -> pd.DataFrame:
+    """Find positions given bollinger bands
 
+    Args:
+        df (pd.DataFrame): A dataframe of stock price data containing at least columns for upper and lower bands, and close prices.
 
+    Returns:
+        pd.DataFrame: The same dataframe as `df` but with one added column 'pos_type' indicating whether the position was identified as a 'buy' or 'sell'.
+    """
+    # initialize    
+    bars['pos_type'] = 'none'
+    # criteria
+    # find points where the close was above the upper band; these may be points at which you'd want to sell the stock
+    # find points where the close was below the lower band; these may be points at which you'd want to buy the stock
 
+    # find sell positions
+    mask = (bars['close'] > bars['upper_band'])
+    bars['pos_type'] = np.where(mask, bars['pos_type'], 'sell')
+    # find buy positions
+    mask = (bars['close'] > bars['upper_band'])
+    bars['pos_type'] = np.where(~mask, bars['pos_type'], 'buy')
+    return bars
 
+bars = find_bband_positions(bars)
+bars
 
-
-# find historical points where the open was below the lower band
-# these may be points at which you'd want to buy the stock
-buys = {
-    'pos_type': [],
-    'timestamp_in': [],
-    'close': [],
-    'upper': [],
-    'timestamp_out': [],
-    'profit': []
-}
-for i in range(len(bars['open'])):
-    if bars['open'][i] < bars['lower_band'][i]:
-        myclose = bars['close'][i]
-        myband = bars['upper_band'][i]
-        mytimestamp = bars['timestamp'][i]
-        
-        buys['pos_type'].append('buy')
-        buys['timestamp'].append(mytimestamp)
-        buys['close'].append(myclose)
-        buys['upper'].append(myband)
-
-        # hold until the price stops going up
-        # calculate slope between two points
-        # if the slope is positive, hold
-        # if the slope is 0 or negative , check the next candle
-        # if the slope to the next candle is also 0 or negative, sell
-        # on sell, capture out timestamp and calculate profit
+def find_volume_positions(df:pd.DataFrame) -> pd.DataFrame:
+    pass
